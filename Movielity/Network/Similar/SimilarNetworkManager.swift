650d7b8e6f1fecd50947721500c101090ee619e6
rkdtnlzl <rkdtnlzl@naver.com>
2024-10-11 21:48:12 +0900

    [FEAT/#12] 비슷한 TV API 구현

--- Movielity/Network/Similar/SimilarNetworkManager.swift
+++ Movielity/Network/Similar/SimilarNetworkManager.swift
@@ -37,4 +37,25 @@ final class SimilarNetworkManager {
         }
     }
     
+    func similarSeries(series_id: String) -> Observable<SimilarSeries> {
+        return Observable.create { observer in
+            do {
+                let request = try Router.similarSeries(series_id: series_id).asURLRequest()
+                
+                AF.request(request)
+                    .responseDecodable(of: SimilarSeries.self) { response in
+                        switch response.result {
+                        case .success(let success):
+                            observer.onNext(success)
+                            observer.onCompleted()
+                        case .failure(let failure):
+                            observer.onError(failure)
+                        }
+                    }
+            } catch {
+                observer.onError(error)
+            }
+            return Disposables.create()
+        }
+    }
 }
